b74c60926893c063f7069aa167c9d7eb1ca5fa04
SamG82 <samg13579@gmail.com>
2019-06-11 22:09:17 -0400

    Removed redundant modelNames array

--- router.js
+++ router.js
@@ -6,20 +6,13 @@ const Router = express.Router()
 //Creating all of these here so that constant, redundant data isn't appended to them every time that they're needed
 let models = []
 let modelPromises = []
-let modelNames = []
-
-let format = (modelName) => {
-    return modelName + 's'
-}
 
 const Conductor = {
     applyModels: function (inputModels) {
         models = inputModels
         models.forEach(model => {
             modelPromises.push(model.find({}))
-            modelNames.push(format(model.modelName))
         })
-
     },
     Router
 }
@@ -38,6 +31,7 @@ Router.get('/', (req, res) => {
             console.log(err)
         })
 })
+
 
 Object.seal(Conductor)
 
